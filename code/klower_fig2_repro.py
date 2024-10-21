0a6f4a64f90efdc67f15355f830fc143245740f8
kllewers <kristen.lewers@gmail.com>
2024-10-21 17:01:04 -0600

    adding in Klower figure code

---
+++ code/klower_fig2_repro.py
@@ -0,0 +1,14 @@
+import xbitinfo as xb
+import numpy as np
+import pandas as pd
+import julia
+import os
+
+
+#Parse the header and binary files
+
+#Get bit information about the files
+
+#Run xbitinfo
+
+#Make figure to look at loss/compression
